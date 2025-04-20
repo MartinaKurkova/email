9f18f15d04d50c32b5cc66ef45df28fd65c06487
Martina Kurkova <martinanoha@seznam.cz>
2025-04-20 17:20:44 +0200

    ukol2

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ const goodbye = (name) => {
 }
 
 goodbye("Pavel Ovesný");
+
+const fillSubject = (subject) => {
+    const subjectEl = document.querySelector(".email__subject");
+    subjectEl.textContent = `${subject}`;
+}
+
+fillSubject("junior frontend vývojářka");
+
+const fillBody = (body, name) => {
+    const bodyEl = document.querySelector(".email__body");
+    bodyEl.textContent = `${body}`;
+    goodbye("Pavel Ovesný")
+}
+
+fillBody("odpovídám na váš inzerát ohledně nabídky práce.");
